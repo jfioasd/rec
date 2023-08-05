e52f4dad182850af60f12facc01babc7622019a5
jfioasd <120231014+jfioasd@users.noreply.github.com>
2023-08-05 21:04:55 +0800

    Update range.r

--- ex/range.r
+++ ex/range.r
@@ -1,8 +1,3 @@
 32 47 [b 1:P 1:1:[0:^\1:\1;] b[^]^ 1:/1;]
 
-[ 0^
-(P.S. While debugging the program `32 47 [b 1:P 1:1:[0:^\1:\1;] b[^]^ 1:/1;]`,
-I found some inconveniences of the debugger, so that's why I added the `c` command.
-It turned out to be a bug of the implementation of `^`, which jumps over a character
-with the for loop increment.)
-]
+[ 0^ This also showcases the debugger. ]
